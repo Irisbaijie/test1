633d2d0b0f06747834a1000231ebb68da9ddb4ce
Irisbaijie <42249939+Irisbaijie@users.noreply.github.com>
2018-08-13 13:16:29 -0400

    Add files via upload

---
+++ class test.py
@@ -0,0 +1,33 @@
+# a class sample copied from network
+class A(object):
+    def __init__(self):
+        self.x = 'Hello'
+
+    def out(self, foo):
+        print self.x + ' ' + foo
+
+
+a = A()             # We do not pass any argument to the __init__ method
+a.out('Sailor!')    # We only pass a single argument
+
+
+# a test code for Class ans Object
+class myObj:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+    def out(self):
+        print('ID is: ' + str(self.id) + ', Name is ' + self.name)
+
+
+obj = myObj(1, 'this')
+obj.out()
+
+
+class Shark:
+    def __init__(self):
+        print("This is the constructor method.")
+
+
+sh = Shark()
---
+++ test.py
@@ -0,0 +1,11 @@
+x = 0
+if x == 0:
+    print ('zero')
+elif x == 1:
+    print ('single')
+else:
+    print ('more')
+print("Hello World")
+print("My name is")  # add your name after the word "is" obviously
+str1 = 'Bob'.upper()
+print(str1)
